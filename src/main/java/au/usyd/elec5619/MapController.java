df1a71942bfd51ff2bb16a9040d6be7ecb6a1b42
Anqi Chen <anqichen@seas.upenn.edu>
2018-10-16 22:43:28 -0700

    Remove logger in MapController

--- src/main/java/au/usyd/elec5619/MapController.java
+++ src/main/java/au/usyd/elec5619/MapController.java
@@ -1,8 +1,5 @@
 package au.usyd.elec5619;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value="/map")
 public class MapController {
-	protected final Log logger = LogFactory.getLog(getClass());
 
 	@RequestMapping(value={"", ".htm", ".html"}, method=RequestMethod.GET)
 	public String showStudent(Model uiModel) {
